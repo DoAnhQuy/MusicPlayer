69b542bc98ae97653332b26ac6e7a9c463c9e84a
DoAnhQuy <doanhquy18062003@gmail.com>
2025-10-31 19:52:37 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.musicPlayer"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.musicPlayer"
+        minSdk = 22
+        targetSdk = 34
+        versionCode = 11
+        versionName = "2.0.1"
+
+        buildConfigField("String", "VERSION_NAME", "\"$versionName\"")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures{
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // Pull to Refresh
+    implementation(libs.legacy.support)
+
+    // Glide for image loading
+    implementation(libs.glide)
+
+    // For storing objects in shared preferences
+    implementation(libs.gson)
+
+    // Notification
+    implementation(libs.androidx.media)
+
+    // Vertical Seekbar
+    implementation(libs.verticalseekbar)
+
+}
